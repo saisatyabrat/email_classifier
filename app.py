3733ebf97779e4150d2e033444896d0908ae8fd3
Sai Satyabrat Biswal <121072358+saisatyabrat@users.noreply.github.com>
2025-04-21 16:20:45 +0530

    app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-import gradio as gr  # create web-based user interfaces 
-from utils import mask_pii  #handles detection and masking of PII in the email text
-from models import predict_category  #function classifies the email text into a category
+import gradio as gr  # Create web-based user interfaces
+from utils import mask_pii  # Handles detection and masking of PII in the email text
+from models import predict_category  # Function classifies the email text into a category
 
-# function logic that Gradio can use
+# Function logic that Gradio can use
 def classify_email(email_text):
-    # Calls the mask_pii function on the input text to give entities , masked_email
+    # Calls the mask_pii function on the input text to get masked_email and entities
     masked_email, entities = mask_pii(email_text)
     # Classifies the masked email using the model
     category = predict_category(masked_email)
-    # Returns all the relevant results in a dictionary, which Gradio will format as JSON format o/p
+    # Returns all the relevant results in a dictionary (Gradio will format this as JSON)
     return {
         "input_email_body": email_text,
         "list_of_masked_entities": entities,
@@ -16,15 +16,16 @@ def classify_email(email_text):
         "category_of_the_email": category
     }
 
-# creates the Gradio UI
+# Create the Gradio UI
 iface = gr.Interface(
-    # use classify_email when the user submits input
     fn=classify_email,
-    # text box (12 lines) for users to paste the email text
-    inputs=gr.Textbox(lines=12, placeholder="Paste support email here..."),
-    # output is shown in JSON format for readability
+    inputs=gr.Textbox(
+        lines=12,
+        placeholder="Paste support email here...",
+        label="Support Email",
+        info="Please make sure names start with a capital letter and use your first name Example:- Mr. Jhon ,Sai"
+    ),
     outputs="json",
-    #  title and description for the Gradio app
     title="Email Classifier for Support Team",
     description="This app masks PII and classifies support emails into categories like Billing, Tech Support, etc."
 )
